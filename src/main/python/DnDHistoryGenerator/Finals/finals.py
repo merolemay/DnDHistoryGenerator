2e09c9b80377cc7639e3a98190831cfd761c4951
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2023-09-07 23:12:27 -0500

    Added Class Structure

---
+++ src/main/python/DnDHistoryGenerator/Finals/finals.py
@@ -0,0 +1,4 @@
+class Finals:
+    def obtener_final(self, eleccion_usuario):
+        # Lógica para determinar el final de la historia
+        pass
---
+++ src/main/python/DnDHistoryGenerator/History/transductors.py
@@ -0,0 +1,4 @@
+class Transductors:
+    def personalizar_historia(self, historia, usuario):
+        # Lógica para personalizar la historia según las preferencias del usuario
+        pass
