0eb0bdf597fc7d15e0fed655b3f2afa7aaf200ed
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2023-09-08 14:27:13 -0500

    Added some logic to the UI in the main class

---
+++ src/main/python/DnDHistoryGenerator/History/automatas.py
@@ -0,0 +1,7 @@
+class Automatas:
+    def __init__(self):
+        self.estado_actual = 0
+
+    def avanzar(self, eleccion):
+        # Lógica para avanzar en la historia basada en la elección del usuario
+        pass
---
+++ src/main/python/DnDHistoryGenerator/History/grammar.py
@@ -0,0 +1,4 @@
+class Gramaticas:
+    def generar_dialogo(self):
+        # Lógica para generar diálogos entre personajes
+        pass
