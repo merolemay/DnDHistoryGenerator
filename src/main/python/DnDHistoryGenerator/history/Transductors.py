f96372b745285c6ce724a9453f9f6a27d2413bd8
Duvan R. Cuero <47835668+merolemay@users.noreply.github.com>
2023-09-21 16:28:13 -0500

    Update Transductors.py to implement the negative & positive reaction

--- src/main/python/DnDHistoryGenerator/history/Transductors.py
+++ src/main/python/DnDHistoryGenerator/history/Transductors.py
@@ -3,7 +3,7 @@ from pyformlang.fst import FST
 class TransductorParty:
     fst = FST()
     
-    def partyPostiveReaction(self) 
+    def partyPostiveReaction(self):
         input_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
         output_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
         states = {"q0","q1","q2","q3","q4","q5"}
@@ -11,13 +11,28 @@ class TransductorParty:
         final_states = {"q1","q2","q3","q4","q5"}
         transitions = {[
             ("q0","Fight","q1",["We shall fight by your side!"]),
-
-
+            ("q0","Talk","q2",["We shall talk to them!"]),
+            ("q0","Decieve","q3",["We shall deceieve them!"]),
+            ("q0","Hide","q4",["We shall hide!"]),
+            ("q0","Dodge","q5",["We shall avoid this with you!"]),
         ]}
         fst = FST(input_simbols, output_simbols, states, initial_state, final_states, transitions)
         return fst
 
-    def partyNegativeReaction(self)
+    def partyNegativeReaction(self):
+        input_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
+        output_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
+        states = {"q0","q1","q2","q3","q4","q5"}
+        initial_state = "q0"
+        final_states = {"q1","q2","q3","q4","q5"}
+        transitions = {[
+            ("q0","Fight","q1",["We shall not fight by your side!"]),
+            ("q0","Talk","q2",["We shall not talk to them!"]),
+            ("q0","Decieve","q3",["We shall not deceieve them!"]),
+            ("q0","Hide","q4",["We shall not hide!"]),
+            ("q0","Dodge","q5",["We shall not avoid this with you!"]),
+        ]}
+        fst = FST(input_simbols, output_simbols, states, initial_state, final_states, transitions)
+        return fst
          
    
-    
