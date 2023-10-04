e0183d24753cbbcb12ce0e1438fc98f8941c522b
Alejandra Díaz <47869960+trp216@users.noreply.github.com>
2023-10-04 16:58:16 -0500

    Fixed a typo

--- src/main/python/DnDHistoryGenerator/history/Transductors.py
+++ src/main/python/DnDHistoryGenerator/history/Transductors.py
@@ -4,15 +4,15 @@ class TransductorParty:
     fst = FST()
     
     def partyPostiveReaction(self):
-        input_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
-        output_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
+        input_simbols={"Fight","Talk","Deceive","Hide","Dodge"}
+        output_simbols={"Fight","Talk","Deceive","Hide","Dodge"}
         states = {"q0","q1","q2","q3","q4","q5"}
         initial_state = "q0"
         final_states = {"q1","q2","q3","q4","q5"}
         transitions = {[
             ("q0","Fight","q1",["We shall fight by your side!"]),
             ("q0","Talk","q2",["We shall talk to them!"]),
-            ("q0","Decieve","q3",["We shall deceieve them!"]),
+            ("q0","Deceive","q3",["We shall deceieve them!"]),
             ("q0","Hide","q4",["We shall hide!"]),
             ("q0","Dodge","q5",["We shall avoid this with you!"]),
         ]}
@@ -20,15 +20,15 @@ class TransductorParty:
         return fst
 
     def partyNegativeReaction(self):
-        input_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
-        output_simbols={"Fight","Talk","Decieve","Hide","Dodge"}
+        input_simbols={"Fight","Talk","Deceive","Hide","Dodge"}
+        output_simbols={"Fight","Talk","Deceive","Hide","Dodge"}
         states = {"q0","q1","q2","q3","q4","q5"}
         initial_state = "q0"
         final_states = {"q1","q2","q3","q4","q5"}
         transitions = {[
             ("q0","Fight","q1",["We shall not fight by your side!"]),
             ("q0","Talk","q2",["We shall not talk to them!"]),
-            ("q0","Decieve","q3",["We shall not deceieve them!"]),
+            ("q0","Deceive","q3",["We shall not deceieve them!"]),
             ("q0","Hide","q4",["We shall not hide!"]),
             ("q0","Dodge","q5",["We shall not avoid this with you!"]),
         ]}
