bf65992d36696459d060b0b6c5ebb54518ec449e
Alejandra Díaz <47869960+trp216@users.noreply.github.com>
2023-10-04 21:27:09 -0500

    Create testchoices

---
+++ src/main/python/DnDHistoryGenerator/test/TestChoices.py
@@ -0,0 +1,32 @@
+# test all the methods in class Choices from package interaction using unit test
+import unittest
+from DnDHistoryGenerator.interaction.Choices import Choices
+
+class TestChoices(unittest.TestCase):
+
+    def setUp(self):
+        # initialize the class
+        self.choices = Choices()
+        
+    def test_present_choices(self):
+        self.assertTrue(True)
+
+    def test_validateInputRegrex(self):
+        self.assertTrue(self.choices.validateInputRegrex("Fight"))
+        self.assertTrue(self.choices.validateInputRegrex("Talk"))
+        self.assertTrue(self.choices.validateInputRegrex("Deceive"))
+        self.assertTrue(self.choices.validateInputRegrex("Hide"))
+        self.assertTrue(self.choices.validateInputRegrex("Dodge"))
+        self.assertTrue(self.choices.validateInputRegrex("fight"))
+        self.assertTrue(self.choices.validateInputRegrex("talk"))
+        self.assertTrue(self.choices.validateInputRegrex("deceive"))
+        self.assertTrue(self.choices.validateInputRegrex("hide"))
+        self.assertTrue(self.choices.validateInputRegrex("dodge"))
+
+        self.assertFalse(self.choices.validateInputRegrex("f"))
+        self.assertFalse(self.choices.validateInputRegrex("t"))
+        self.assertFalse(self.choices.validateInputRegrex("d"))
+        self.assertFalse(self.choices.validateInputRegrex("fighting"))
+
+if __name__ == '__main__':
+    unittest.main()
