d226eed8976e9adfcb5402121f1684d862f4562c
Alejandra Díaz <47869960+trp216@users.noreply.github.com>
2023-10-05 00:04:54 -0500

    Added test for characters

---
+++ src/main/test/TestCharacters.py
@@ -0,0 +1,15 @@
+# # test all the methods in class characters from package interaction using unit test
+import unittest
+from characters.Characters import Character
+
+class TestCharacters(unittest.TestCase):
+    
+        def setUp(self):
+            # initialize the class
+            self.character = Character()
+            self.character.generate_character()
+            
+        def test_generate_character(self):
+            self.assertTrue(len(self.character.name) > 0)
+            self.assertTrue(len(self.character.race) > 0)
+            self.assertTrue(len(self.character.alignment) > 0)
--- src/main/test/TestChoices.py
+++ src/main/test/TestChoices.py
@@ -7,11 +7,9 @@ class TestChoices(unittest.TestCase):
     def setUp(self):
         # initialize the class
         self.choices = Choices()
-        
-    def test_present_choices(self):
-        self.assertTrue(True)
 
     def test_validateInputRegrex(self):
+        # Validate for correct input
         self.assertTrue(self.choices.validateInputRegrex("Fight"))
         self.assertTrue(self.choices.validateInputRegrex("Talk"))
         self.assertTrue(self.choices.validateInputRegrex("Deceive"))
@@ -23,10 +21,24 @@ class TestChoices(unittest.TestCase):
         self.assertTrue(self.choices.validateInputRegrex("hide"))
         self.assertTrue(self.choices.validateInputRegrex("dodge"))
 
+        # Validate for incorrect input
         self.assertFalse(self.choices.validateInputRegrex("f"))
         self.assertFalse(self.choices.validateInputRegrex("t"))
         self.assertFalse(self.choices.validateInputRegrex("d"))
         self.assertFalse(self.choices.validateInputRegrex("fighting"))
+
+    def test_validateInputYesorNo(self):
+        # Validate for correct input
+        self.assertTrue(self.choices.validateInputYesorNo("Y"))
+        self.assertTrue(self.choices.validateInputYesorNo("N"))
+        self.assertTrue(self.choices.validateInputYesorNo("y"))
+        self.assertTrue(self.choices.validateInputYesorNo("n"))
+
+        # Validate for incorrect input
+        self.assertFalse(self.choices.validateInputYesorNo("Yes"))
+        self.assertFalse(self.choices.validateInputYesorNo("No"))
+        self.assertFalse(self.choices.validateInputYesorNo("yes"))
+        self.assertFalse(self.choices.validateInputYesorNo("no"))
 
 if __name__ == '__main__':
     unittest.main()
