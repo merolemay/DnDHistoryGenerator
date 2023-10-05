462209322c9c77fc75019da6987491fdedd5eb8a
Alejandra Díaz <47869960+trp216@users.noreply.github.com>
2023-10-05 00:27:16 -0500

    Create TestGrammar.py

---
+++ src/main/test/TestGrammar.py
@@ -0,0 +1,32 @@
+import unittest
+from history.Grammar import Grammar
+from characters.Characters import Character
+
+class TestGrammar(unittest.TestCase):
+    def setUp(self):
+        # initialize the class
+        self.grammar = Grammar()
+
+        party = []
+        
+        self.character = Character()
+        self.character.name = "LuciusShadowflame"
+        self.character.race = "Elf"
+        self.character.alignment = "Lawful-Good"
+
+        party.append(self.character)
+
+        self.character.name = "DamienBloodthorn"
+        self.character.race = "Human"
+        self.character.alignment = "True-Neutral"
+
+        party.append(self.character)
+
+        
+
+    def test_generate_description(self):
+        # Validate for correct input
+        self.assertEqual(self.grammar.generate_description(self.party), "\na Elf of Lawful-Good alignment named LuciusShadowflame, \na Human of True-Neutral alignment named DamienBloodthorn.")
+
+if __name__ == '__main__':
+    unittest.main()
